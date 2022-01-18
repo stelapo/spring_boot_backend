4e16ec97b60b0d3d60fac4f4193954e15f97aae2
Lapo Pancani <stelapo@gmail.com>
2022-01-18 14:25:48 +0100

    Aggiunta classe repository "EmployeeRepository"

---
+++ src/main/java/com/example/spring_boot_backend/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.spring_boot_backend.repository;
+
+import com.example.spring_boot_backend.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
